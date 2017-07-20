78e8fc124a25d3e90ba49b56ce392e9ec9523165
Marguerite Cramer <margueritecramer@Marguerites-MacBook-Air.local>
2017-07-20 15:14:02 -0400

    update API integration

--- app/controllers/api/v1/commutes_controller.rb
+++ app/controllers/api/v1/commutes_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::CommutesController < ApplicationController
   def index
     render json: Commute.all
   end
+
+  def show
+    @commute = { commute: Commute.find(params[:id])}
+    @reviews = { reviews: Review.where(commute_id: params[:id]) }
+
+    commute_reviews = @commute.merge(@reviews)
+    render json: commute_reviews, adapter: :json
+  end
 end
