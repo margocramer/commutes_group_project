ba742f44fdd75fe0f9fa1fdcd8a72f74ff1f74eb
Marguerite Cramer <margueritecramer@Marguerites-MacBook-Air.local>
2017-07-24 15:03:11 -0400

    factoryGirl and draft Karma tests

--- spec/factories/commutes.rb
+++ spec/factories/commutes.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :commute do
-    
+    starting_location "Cambridge"
+    ending_location "Boston"
+    time "Morning"
+    mode "Train"
+    description "This was my commute and it was awesome!!!"
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    username "freefall"
+    first_name "free"
+    last_name "fall"
+    email "free@fall.com"
+    password "thisisalongpassword"
+
   end
 end
