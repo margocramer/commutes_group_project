1e4b12362e9d8497070065e8ad4c84858a3a7b04
Michelle Nosal <nosalcontact@yahoo.com>
2017-07-27 14:04:22 -0400

    success for editing and removing reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,10 +13,8 @@ class ReviewsController < ApplicationController
       @review.user = current_user
 
       if @review.save
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        #add Mailer here
         ReviewMailer.receipt.deliver_now
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
         redirect_to root_path,
         notice: "Review added successfully!"
       else
@@ -26,6 +24,28 @@ class ReviewsController < ApplicationController
       redirect_to root_path,
       alert: "You cannot add reviews until you are signed in"
     end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+    @commute = @review.commute
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @commute = @review.commute
+    if @review.update(review_params)
+      redirect_to @commute, notice: 'Review was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @commute = @review.commute
+    @review.destroy
+    redirect_to root_path, notice: 'Your review was successfully destroyed.'
   end
 
   private
