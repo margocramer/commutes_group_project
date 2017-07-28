b7c921c9114a99f81adcf81112c6a42eddd02eff
Michelle Nosal <nosalcontact@yahoo.com>
2017-07-28 07:01:15 -0400

    fix spec load issue

--- spec/mailers/review_mailer_spec.rb
+++ spec/mailers/review_mailer_spec.rb
@@ -1,17 +1,18 @@
-require "rails_helper"
+require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe ReviewMailer, type: :mailer do
   describe "receipt" do
     let(:mail) { ReviewMailer.receipt }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Receipt")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("New for review for your commute")
+      expect(mail.to).to eq(["smnrwebster5@gmail.com"])
+      expect(mail.from).to eq(["GettingSquirrely@weird.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to include("Someone has left a new review for your commute!!")
     end
   end
 
